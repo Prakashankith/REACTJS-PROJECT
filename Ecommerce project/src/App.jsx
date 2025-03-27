afb3d7bb96484a63dfd6f4c004a5c2a89a9b9a2d
Prakashankith <sirankithbp1999@gmail.com>
2025-03-27 22:30:32 +0530

    added a responsive navbar

---
+++ Ecommerce project/src/App.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home";
+import Mens from "./Pages/Mens";
+import Womens from "./Pages/Womens";
+import Kids from "./Pages/Kids";
+import Login from "./Pages/Login";
+import Cart from "./Pages/Cart";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <Home />
+      </>
+    ),
+  },
+  {
+    path: "/mens",
+    element: (
+      <>
+        <Navbar />
+        <Mens />
+      </>
+    ),
+  },
+  {
+    path: "/womens",
+    element: (
+      <>
+        <Navbar />
+        <Womens />
+      </>
+    ),
+  },
+  {
+    path: "/kids",
+    element: (
+      <>
+        <Navbar />
+        <Kids />
+      </>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <>
+        <Navbar />
+        <Login />
+      </>
+    ),
+  },
+  {
+    path: "/cart",
+    element: (
+      <>
+        <Navbar />
+        <Cart />
+      </>
+    ),
+  },
+]);
+const App = () => {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default App;
---
+++ Ecommerce project/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroBanner from "../Components/Herobanner";
+
+const Home = () => {
+  return (
+    <>
+      <HeroBanner />
+    </>
+  );
+};
+
+export default Home;
