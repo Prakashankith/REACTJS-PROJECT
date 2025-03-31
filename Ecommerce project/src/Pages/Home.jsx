8e632ac2e48f8e0b19d65baff48c9ef5483625df
Prakashankith <sirankithbp1999@gmail.com>
2025-03-31 23:00:27 +0530

    changed singleproduct

--- Ecommerce project/src/Pages/Home.jsx
+++ Ecommerce project/src/Pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import HeroBanner from "../Components/Herobanner";
-import MultiBanner from "../Components/Multibanner";
-import NewCollection from "../Components/NewCollection";
-import MidBanner from "../Components/MidBanner";
-import TopSellers from "../Components/TopSellers";
-import Features from "../Components/Features";
+import HeroBanner from "../components/HeroBanner";
+import MultiBanner from "../components/MultiBanner";
+import NewCollection from "../components/NewCollection";
+import MidBanner from "../components/MidBanner";
+import TopSellers from "../components/TopSellers";
+import Features from "../components/Features";
 
 const Home = () => {
   return (
