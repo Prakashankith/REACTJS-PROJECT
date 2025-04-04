517b53510bddd8b0e71b0f2801cf5ad280166e67
Prakashankith <sirankithbp1999@gmail.com>
2025-04-04 21:30:23 +0530

    added a header

--- Movie App/src/App.jsx
+++ Movie App/src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div className="bg-red-800">App</div>;
+  return (
+    <main>
+      <Header />
+      <div className="pt-16">
+        <Outlet />
+      </div>
+      <Footer />
+    </main>
+  );
 };
 
 export default App;
---
+++ Movie App/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import logo from "../assets/logo.png";
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 w-full h-16 bg-black bg-opacity-50 z-40">
+      <div className="container mx-auto px-3 flex items-center h-full">
+        <Link to={"/"}>
+          <img src={logo} alt="logo" width={120} />
+        </Link>
+
+        <nav className="hidden lg:flex items-center gap-1 ml-5">
+          {navigation.map((nav, index) => {
+            return (
+              <div>
+                <NavLink
+                  key={nav.label + "header" + index}
+                  to={nav.href}
+                  className={({ isActive }) =>
+                    `px-2 hover:text-neutral-100 ${
+                      isActive && "text-neutral-100"
+                    }`
+                  }
+                >
+                  {nav.label}
+                </NavLink>
+              </div>
+            );
+          })}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
